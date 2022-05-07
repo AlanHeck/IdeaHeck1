a68ecf2947180e0ea79275afd932174988488022
AlanHeck <100172228+AlanHeck@users.noreply.github.com>
2022-05-06 22:13:35 -0300

    formatear codigo

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ const campoMonto = document.getElementById("monto");
 const botonRadioDolar = document.getElementById("cambio-dolar");
 const botonRadioEuro = document.getElementById("cambio-euro");
 
-function convertir () {
+function convertir() {
     let montoComoNumero = parseInt(campoMonto.value);
     let resutaldo = 0;
-    if(isNaN(montoComoNumero)){
+    if (isNaN(montoComoNumero)) {
         alert("Debes completar todos los requerimientos")
     }
-    else{
-        if(botonRadioDolar.checked){
-            resutaldo = montoComoNumero / pesosPorDolar; 
+    else {
+        if (botonRadioDolar.checked) {
+            resutaldo = montoComoNumero / pesosPorDolar;
             alert("El cambio de pesos a dolares es: $" + resutaldo.toFixed(2));
         }
-        else if (botonRadioEuro.checked){
-            resutaldo = montoComoNumero / pesosPorEuro; 
+        else if (botonRadioEuro.checked) {
+            resutaldo = montoComoNumero / pesosPorEuro;
             alert("El cambio de pesos a euros es: $" + resutaldo.toFixed(2));
         }
-        else{
+        else {
             alert("Debes completar todos los requerimientos")
         }
     }
