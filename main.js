a884effd4ef603bfcbf053fafd3d9e5c9d76e0e7
AlanHeck <100172228+AlanHeck@users.noreply.github.com>
2022-05-06 21:45:34 -0300

    agregar validacion de monto

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ function convertir () {
     let resutaldo = 0;
     let dolar = 205;
     let euro = 210;
-    if(document.getElementById("uno").checked){
-        resutaldo = valore / dolar; 
-        alert("El cambio de pesos a dolares es: $" + resutaldo.toFixed(2));
-    }
-    else if (document.getElementById("dos").checked){
-        resutaldo = valore / euro; 
-        alert("El cambio de pesos a euros es: $" + resutaldo.toFixed(2));
+    if(isNaN(valore)){
+        alert("Debes completar todos los requerimientos")
     }
     else{
-        alert("Debes completar todos los requerimientos")
+        if(document.getElementById("uno").checked){
+            resutaldo = valore / dolar; 
+            alert("El cambio de pesos a dolares es: $" + resutaldo.toFixed(2));
+        }
+        else if (document.getElementById("dos").checked){
+            resutaldo = valore / euro; 
+            alert("El cambio de pesos a euros es: $" + resutaldo.toFixed(2));
+        }
+        else{
+            alert("Debes completar todos los requerimientos")
+        }
     }
 }
